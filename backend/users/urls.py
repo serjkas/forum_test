46fa874660d725d8e18292c249c551a12bb586cf
serjkas <serjkasmail@gmail.com>
2021-06-24 21:51:33 +0300

    fix urls start frontend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_users_forum
 
 urlpatterns = [
-    path('', get_users_forum),
+    path('get_users_forum', get_users_forum),
 ]
---
+++ frontend/src/components/Users.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+        <tr>
+            <th>ScriptName</th>
+            <th>EffortsCount</th>
+            <th>ConnectionsCount</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="report in reports" :key="report.id">
+            <td>{{ report.scriptname}}</td>  
+            <td>{{ report.effortscount}}</td>  
+            <td>{{ report.connectionscount}}</td>  
+
+        </tr>
+        
+        </tbody>
+    </table>
+    </div>
+
+</template>
